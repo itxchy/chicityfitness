68dac88d611be4e3b81658e4042b2d132d684c91
itxchy <matt.trifilo@gmail.com>
2015-07-29 06:58:32 -0500

    Fixed header slideshow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 /* jshint devel:true */
 'use strict';
-
+/*
 function cycleImages() {
   var $active = $('.headerPhoto .active');
   var $next = ($active.next().length > 0) ? $active.next() : $('.headerPhoto img:first');
@@ -9,9 +9,24 @@ function cycleImages() {
     $active.css('z-index', 1).show().removeClass('active'); // reset the z-index and unhide the image
     $next.css('z-index', 3).addClass('active'); // make the next image the top one
   });
+} */
+
+//Thank you @JAM from Stack Overflow!
+
+var imageIndex = 0;
+var imagesArray = [
+  '/images/header1.jpg',
+  '/images/header2.jpg',
+  '/images/header3.jpg',
+  '/images/header4.jpg'
+];
+
+function headerSlides() {
+  var index = imageIndex++ % imagesArray.length;
+  $('.headerPhoto').css('background', 'url(\'' + imagesArray[index] + '\') no-repeat center center fixed');
 }
 
 $(document).ready(function() {
-  // run every 7 seconds
-  setInterval('cycleImages()', 3000);
-})
+
+  setInterval(headerSlides, 3000);
+});
